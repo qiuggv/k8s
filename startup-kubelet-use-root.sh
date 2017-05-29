f1676f665b60f10d2c667aaddaf2e6ecbfcb561c
qiuggv <274429987@qq.com>
2017-05-29 10:31:51 +0800

    add skydns

--- startup-kubelet-use-root.sh
+++ startup-kubelet-use-root.sh
@@ -4,4 +4,6 @@ kubelet \
 --api-servers=http://kube-master:8080 \
 --address=0.0.0.0 \
 --config=/etc/kubernetes/manifests \
+--cluster-dns=10.254.10.2 \
+--cluster-domain=cluster.local \
 >> kubelet.log 2>&1 &
